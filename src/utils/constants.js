ba3be7467da9070e40394c8a6da4e95205274cf4
Prajuwal <prajuwalsingh@gmail.com>
2023-09-18 22:34:52 +0530

    Remove constants.js from version control

--- src/utils/constants.js
+++
@@ -1,5 +0,0 @@
-const YOUTUBE_API_KEY = "AIzaSyAtcI399zRTFZdggXC8fHfVeMls-w9yemA";
-
- export const YOUTUBE_API_URL =
-   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key=" +
-   YOUTUBE_API_KEY;
